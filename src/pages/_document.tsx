78175e524cccee410158356d6efecf86174ed3db
oigabrielteodoro <oi@gabrielteodoro.com>
2021-05-24 16:34:21 -0300

    feat: created favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,12 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" />
 
           <link
+            rel="shortcut icon"
+            href="/static/favicon.png"
+            type="image/png"
+          />
+
+          <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
             rel="stylesheet"
           />
